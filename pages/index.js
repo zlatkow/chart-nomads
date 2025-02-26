08508491788ac3a44c1b4e6abadcb4a58c1663b4
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-02-26 18:52:11 +0200

    Updated Supabase client setup

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Scrollable from "../components/Scrollable";
