3b44521f08f11458b49b8957ec876851adc0c4a1
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-03-20 20:01:53 +0200

    Trying more vercel errors fixes

--- pages/banned-countries.js
+++ pages/banned-countries.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
 import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
--- pages/prop-firm-rules.js
+++ pages/prop-firm-rules.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
 import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
