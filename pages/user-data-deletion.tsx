542c52f4cfd00d67ad40515e122edcf9822ccfb5
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-03-26 16:45:43 +0200

    Adding users policy page

---
+++ pages/user-data-deletion.tsx
@@ -0,0 +1,12 @@
+export default function UserDataDeletion() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>User Data Deletion Instructions</h1>
+      <p>If you wish to delete your account or personal data, please contact us at:</p>
+      <p>
+        <strong>Email:</strong> support@chartnomads.xyz
+      </p>
+      <p>We will handle your request within 7 business days.</p>
+    </div>
+  );
+}
