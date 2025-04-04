3395c11bd87d9ebcf6ec807cf639ae0be85c88ae
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-04-04 21:03:35 +0300

    fixing news page

--- components/news-page/table-of-contents.tsx
+++ components/news-page/table-of-contents.tsx
@@ -71,7 +71,7 @@ export function TableOfContents({ articleId }: TableOfContentsProps) {
   }
 
   return (
-    <div className="space-y-2 mt-[200px]">
+    <div className="space-y-2 mt-[50px]">
       <h3 className="font-semibold text-lg text-white">Table of Contents</h3>
       <nav className="space-y-1">
         {headings.map((heading) => (
