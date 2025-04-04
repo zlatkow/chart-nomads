c5e39b352fe2c00cb5199be877ef843cea2c1e2d
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-04-04 11:04:12 +0300

    fixing noise

--- components/Noise.tsx
+++ components/Noise.tsx
@@ -27,7 +27,7 @@ const Noise = ({ isVisible: propIsVisible }: NoiseProps) => {
     return null
   }
 
-  return <div className="fixed inset-0 noise-overlay opacity-[0.7] z-10"></div>
+  return <div className="fixed inset-0 noise-overlay opacity-[0.7]"></div>
 }
 
 export default Noise
