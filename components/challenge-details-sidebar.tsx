9edfff2f4df27bd68ee067d03a34698fe53cc18c
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-04-08 18:18:07 +0300

    fixing challenge sidebar

--- components/challenge-details-sidebar.tsx
+++ components/challenge-details-sidebar.tsx
@@ -240,7 +240,7 @@ export default function ChallengeDetailsSidebar({ challenge, isOpen, onClose }: 
               <div className="flex h-[100px] items-center justify-between p-3 border-b border-[#222]">
                 <div className="flex items-center gap-2">
                   <div
-                    className="w-15 h-15 rounded-md flex items-center justify-center overflow-hidden"
+                    className="w-[60px] h-[60px] rounded-[10px] flex items-center justify-center overflow-hidden"
                     style={{ backgroundColor: challenge.firm.color }}
                   >
                     {challenge.firm.logo ? (
