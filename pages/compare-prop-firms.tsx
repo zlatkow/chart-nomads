b920f31e6cda2694050fd90980d1f41c7e9c373b
Delyan Zlatkov <zlatkowdw@gmail.com>
2025-04-08 15:34:38 +0300

    fixing sticky table

--- pages/compare-prop-firms.tsx
+++ pages/compare-prop-firms.tsx
@@ -1167,6 +1167,7 @@ export default function PropFirmComparison() {
                           <div className="absolute right-0 top-1/4 z-15 h-1/2 w-px bg-[#333]"></div>
                         </th>
                         <th className="p-3 text-center relative">
+                          <div className="absolute right-0 top-1/4 z-15 h-1/2 w-px bg-[#333]"></div>
                           <button
                             onClick={() => handleSort("price")}
                             className="flex items-center justify-center gap-1 w-full hover:text-[#edb900] transition-colors"
